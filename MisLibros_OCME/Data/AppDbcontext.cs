208eef28dbdd4a88f81cd831520580201dca7094
omarcotamedina28@gmail.com <omarcotamedina18@gmail.com>
2024-11-06 13:32:34 -0700

    Agregar archivos de proyecto.

---
+++ MisLibros_OCME/Data/AppDbcontext.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using MisLibros_OCME.Data.Models;
+
+namespace MisLibros_OCME.Data
+{
+    public class AppDbcontext : DbContext
+    {
+        public AppDbcontext(DbContextOptions<AppDbcontext> options) : base(options)
+        {
+
+
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
