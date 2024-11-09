64320c1dca9f5f8de48ae826a42392c41fd445c7
omarcotamedina28@gmail.com <omarcotamedina18@gmail.com>
2024-11-08 21:02:21 -0700

    kj

--- MisLibros_OCME/Controllers/bookscontrollerr.cs
+++ MisLibros_OCME/Controllers/bookscontrollerr.cs
@@ -15,12 +15,41 @@ namespace MisLibros_OCME.Controllers
         {
             _bookservice = bookservice;
         }
+        [HttpGet("get-all-books")]
+        public IActionResult GetAllBooks()
+        {
+            var allbooks = _bookservice.GetAllBks();
+            return Ok(allbooks);
+        }
+
+        [HttpGet("get-book-by-id/{id}")]
+        public IActionResult GetBookById(int id)
+        {
+            var book = _bookservice.GetBookByID(id);
+            return Ok(book);
+        }
 
         [HttpPost("add-book")]
-        public IActionResult Addbook([FromBody]bookVM book)
+        public IActionResult Addbook([FromBody] bookVM book)
         {
             _bookservice.AddBook(book);
             return Ok();
         }
+
+        [HttpPut("update-book-by-id/{id}")]
+        public IActionResult UpdateBookById(int id, [FromBody] bookVM book)
+        {
+            var updateBook = _bookservice.UpdateBookByID(id, book);
+            return Ok(updateBook);
+        }
+
+        [HttpDelete("delete-book-by-id/{id}")]
+        public IActionResult DeleteBookById(int id)
+        {
+           _bookservice.DeletebookById(id);
+           return Ok();
+        }
     }
 }
+
+
--- MisLibros_OCME/Data/services/bookservice.cs
+++ MisLibros_OCME/Data/services/bookservice.cs
@@ -1,6 +1,9 @@
 ﻿using MisLibros_OCME.Data.ViewModels;
 using MisLibros_OCME.Data.Models;
 using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
 
 namespace MisLibros_OCME.Data.services
 {
@@ -32,7 +35,37 @@ namespace MisLibros_OCME.Data.services
             _context.SaveChanges();
 
         }
+        public List<Book> GetAllBks() => _context.Books.ToList();
+        public Book GetBookByID(int bookid) => _context.Books.FirstOrDefault(n => n.id == bookid);
 
-    
+        public Book UpdateBookByID(int bookid, bookVM book)
+        {
+            var _book = _context.Books.FirstOrDefault(n =>n.id == bookid);
+            if(_book !=null )
+            {
+                _book.Titulo = book.Titulo;
+                _book.Descripcion = book.Descripcion;
+                _book.IsRead = book.IsRead;
+                _book.DateRead = book.DateRead;
+                _book.Rate = book.Rate;
+                _book.Genero = book.Genero;
+                _book.Autor = book.Autor;
+                _book.CoverUrl = book.CoverUrl;
+
+                _context.SaveChanges();
+
+            }
+            return _book;
+        }
+        public void DeletebookById(int bookid)
+        {
+            var _book = _context.Books.FirstOrDefault(n => n.id == bookid);
+            if(_book != null )
+            {
+                _context.Books.Remove( _book );
+                _context.SaveChanges();
+            }
+        }
+
     }
 }
